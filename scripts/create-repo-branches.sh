8a3f988d1a7f365b6c43463785308a09683afdf9
davidko <dko@suse.com>
2024-01-11 15:57:46 +0800

    ci: create a gh action for creating repo branches
    Signed-off-by: davidko <dko@suse.com>

---
+++ scripts/create-repo-branches.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o xtrace
+
+if [ "$#" -ne 2 ]; then
+  echo "Illegal number of arguments. source branch and new_branch are required." >/dev/stderr
+  exit 1
+fi
+
+source_branch=$1
+new_branch=$2
+repos_dir=.repos
+
+repos=(
+  "longhorn/longhorn"
+  "longhorn/longhorn-manager"
+  "longhorn/longhorn-instance-manager"
+  "longhorn/longhorn-engine"
+  "longhorn/longhorn-share-manager"
+  "longhorn/backing-image-manager"
+  "longhorn/longhorn-tests"
+  # "longhorn/longhorn-spdk-engine" only needed since GA
+)
+
+function teardown() {
+  rm -rf $repos_dir
+}
+trap teardown EXIT
+
+mkdir -p $repos_dir
+
+pushd $repos_dir
+for repo in "${repos[@]}"; do
+  git clone git@github.com:"${repo}".git
+
+  pushd "${repo##*/}"
+  git checkout -b "$new_branch" origin/"${source_branch}"
+  git push -u origin "$new_branch"
+  popd
+done
+popd
